3adf6f5c7cc36e475f74465f73fd1c3ad190fc2d
vat332 <oporowymakus21@gmail.com>
2023-03-01 16:36:00 +0100

    Lesson 1

---
+++ index.js
@@ -0,0 +1,12 @@
+const nav = (
+  <nav>
+    <h1>OliS</h1>
+    <ul>
+      <li>Cennik</li>
+      <li>O nas</li>
+      <li>Kontakt</li>
+    </ul>
+  </nav>
+);
+
+ReactDOM.render(nav, document.getElementById("navbar"));
