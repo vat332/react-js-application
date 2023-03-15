cacf260ba31f62c3f34a98289e158166528853ed
vat332 <oporowymakus21@gmail.com>
2023-03-15 19:38:59 +0100

    Base project

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,9 @@
 import './App.css';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Card from './components/Card';
-import cardData from './data.js';
 
 function App() {
-  const cardElements = cardData.map(item =>{
-    return ( 
-      <Card
-        key={item.id}
-        item={item}
-        //Można zrobić {...item}, ale trzeba usunąć w komponencie item przy props
-      />
-  )})
   return (
     <div id="root">
-      <Navbar />
-      <Hero />
-      <section className="cards-list">
-        {cardElements}
-      </section>
+      <h1>Hello world!</h1>
     </div>
   )
 };
